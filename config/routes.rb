8ef52b28b9b12c0374860ab589655ba871374384
Cameron <cameron.p.buckingham@gmail.com>
2014-08-04 15:22:54 -0600

    1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "task_lists#index"
+  get "about" => "sessions#about"
   get "signin" => "sessions#new", as: :signin
   post "signin" => "sessions#create"
   get "signout" => "sessions#destroy", as: :signout
+
 end
